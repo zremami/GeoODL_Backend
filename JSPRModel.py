114247d9c8ec27c3526fa80dbf4ba0ec72404a1f
Zahra <emza1011@h-ka.de>
2022-12-24 00:28:04 +0100

    create dateframe and find missing values

---
+++ JSPRModel.py
@@ -0,0 +1,85 @@
+def JSPRMLModel():
+    from WFSRepository import getOdl_1h, getPrecipitation_15min
+    import pandas as pd
+    import numpy as np
+    import sklearn 
+    from sklearn.linear_model import LinearRegression
+
+    data1json=getOdl_1h()
+    data1norm= pd.json_normalize(data1json,"features")
+    df1 = pd.DataFrame(data1norm)
+
+    print(df1)
+    print(df1.sum())
+    print(df1.info())
+
+
+    data2json=getPrecipitation_15min()
+    data2norm= pd.json_normalize(data2json,"features")
+    df2 = pd.DataFrame(data2norm)
+
+    df2['properties.start_measure'] = pd.to_datetime(df2['properties.start_measure']).dt.tz_localize(None)
+    df2['properties.end_measure'] = pd.to_datetime(df2['properties.start_measure']).dt.tz_localize(None)
+
+    print(df2)
+    print(df2.sum())
+    print(df2.info())
+
+    df2 = df2.squeeze()
+    df2.set_index('properties.start_measure', inplace=True)
+    df2 = df2.resample('H').mean()
+
+    print(df2.sum())
+    print(df2)
+    print(df2.info())
+
+    #ss = pd.DataFrame(df2, columns = df2['properties.value'])
+    PRValueLs = np.array(df2['properties.value'])
+    OLValueLs = np.array(df1['properties.value'])
+    print(PRValueLs)
+    print(OLValueLs)
+
+    df1['properties.valuePR'] = PRValueLs
+
+    #df1.insert(loc=1, column="properties.value2",  value=ss)
+
+    print(df1)
+
+    combineODPR = df1[["properties.kenn","properties.start_measure", "properties.value","properties.valuePR"]]
+    print(combineODPR)
+    return combineODPR
+
+    #df = pd.DataFrame(df, columns=['properties.start_measure', 'properties.value'])
+    #display(df['properties.start_measure'].min())
+    #display(df['properties.start_measure'].max())
+    #print(df['properties.start_measure'].loc['2022-11-27'].head(5))
+    #mask = (df['properties.start_measure']>= pd.Timestamp('2022-11-23')) & (df['properties.start_measure'] < pd.Timestamp('2022-11-24'))
+    #display(df.loc[mask])
+
+
+    '''df.set_index('properties.start_measure', inplace=True)
+    display(df.sort_index())
+    print(type(df.index))
+    display(df.at_time('12:05'))
+    display(df.between_time('00:00','02:00'))
+    display(df.sort_index().first('5B'))
+    display(df.sort_index().last('1W'))
+    print(df['properties.value'])
+    print(df)
+    print(df.info())
+    print(df.get(['properties.value']))
+    df = pd.DataFrame(df)
+    df.resample('D',on='properties.start_measure').mean()
+    print(df)
+    #df[df['properties.value'] == 1].resample('D')['properties.value'].mean()
+    #print(df.groupby(df['properties.end_measure']))
+    #df.groupby(df['properties.end_measure']).resample('D')[df['properties.value']].max()
+
+
+    index = pd.date_range('2022-12-04', periods=9, freq='T')
+    series = pd.Series(range(9), index=index)
+    print(series)
+    print(series.resample('3T').sum())
+
+
+    print(df)'''
--- WFSRepository.py
+++ WFSRepository.py
@@ -5,15 +5,17 @@ import xml.dom.minidom # WFS returns GML - this library is used to parse it
 import json
 import pandas as pd
 from IPython.display import display
+from owslib.wfs import WebFeatureService
 
-def getWFSData():
-    from owslib.wfs import WebFeatureService
+#https://www.imis.bfs.de/ogc/opendata/ows
+wfs11 = WebFeatureService(url='https://entw-imis.lab.bfs.de/ogc/opendata/wfs', version='1.1.0')
 
-    wfs11 = WebFeatureService(url='https://www.imis.bfs.de/ogc/opendata/ows', version='1.1.0')
+def getOdl_1h():
+    
 
-    filter1 = PropertyIsGreaterThanOrEqualTo(propertyname='start_measure', literal='2022-12-01')
+    filter1 = PropertyIsGreaterThanOrEqualTo(propertyname='end_measure', literal='2022-01-01')
     #filter1 = PropertyIsBetween(propertyname='start_measure', lower='2022-11-01',upper='2022-11-02')
-    filter2 = PropertyIsLike(propertyname='kenn', literal='141772901',wildCard='*')
+    filter2 = PropertyIsLike(propertyname='id', literal='DEZ2210',wildCard='*')
     #filters3= PropertyIsLike(propertyname='local_authority', literal='Ulm',wildCard='*')
     filters=[filter1,filter2]
 
@@ -21,12 +23,31 @@ def getWFSData():
 
     #odlinfo_timeseries_precipitation_15min
     #odlinfo_timeseries_odl_1h
-    response = wfs11.getfeature(typename='odlinfo_timeseries_precipitation_15min',filter = filterxml,outputFormat='application/json',maxfeatures=900)
+    response = wfs11.getfeature(typename='opendata:public_odl_brutto_1h',filter = filterxml,outputFormat='application/json')
     # convert IO-byte to bytes
     bytesD=bytes(response.read())
     # convert to json
-    data = json.loads(bytesD)
-    return data   
+    data1 = json.loads(bytesD)
+    return data1
+
+def getPrecipitation_15min():
+
+    filter1 = PropertyIsGreaterThanOrEqualTo(propertyname='end_measure', literal='2022-01-01')
+    #filter1 = PropertyIsBetween(propertyname='start_measure', lower='2022-11-01',upper='2022-11-02')
+    filter2 = PropertyIsLike(propertyname='id', literal='DEZ2210',wildCard='*')
+    #filters3= PropertyIsLike(propertyname='local_authority', literal='Ulm',wildCard='*')
+    filters=[filter1,filter2]
+
+    filterxml = etree.tostring(And(operations=filters).toXML()).decode("utf-8")
+
+    #odlinfo_timeseries_precipitation_15min
+    #odlinfo_timeseries_odl_1h
+    response = wfs11.getfeature(typename='opendata:public_precipitation_15min',filter = filterxml,outputFormat='application/json')
+    # convert IO-byte to bytes
+    bytesD=bytes(response.read())
+    # convert to json
+    data2 = json.loads(bytesD)
+    return data2    
 
 """for index in range(len(datanorm)):
 kenn=datanorm['properties.kenn'][index], 
